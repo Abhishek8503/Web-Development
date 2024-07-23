eb6e13785d45a567e6d89124fa60060fccad1310
Abhi <abhishekg9000@gmail.com>
2024-07-23 11:58:23 +0530

    I'm so cooked

--- spotify.js
+++ spotify.js
@@ -25,7 +25,7 @@ async function getSong(folder) {
     let div = document.createElement("div")
     div.innerHTML = response
     let as = div.getElementsByTagName("a")
-    let songs = []
+    songs = []
     for (let index = 0; index < as.length; index++) {
         const element = as[index];
         if (element.href.endsWith(".m4a") || element.href.endsWith(".mp3")) {
@@ -34,31 +34,12 @@ async function getSong(folder) {
 
         }
     }
-    return songs
-
-}
-
-const playMusic = (track, pause = false)=>{
-    // let audio = new Audio("/songs/" + track)
-    currentSong.src = `/${currfolder}/` + track 
-    if(!pause){
-        currentSong.play()
-        play.src = "pause.svg"
-    }
-    document.querySelector(".songinfo").innerHTML = decodeURI(track)
-    document.querySelector(".songtime").innerHTML = "00:00 / 00:00"
-}
-
-async function main() {    
-    songs = await getSong("songs/MHA")
-    // console.log(songs)
-    playMusic(songs[8], true) 
-
 
     let songUL = document.querySelector(".songList").getElementsByTagName("ul")[0]
     // console.log(songs);
+    songUL.innerHTML = ""
     
-    for (var song of songs) {
+    for (const song of songs) {
         let songName = song.replaceAll("%20", " ")
         // song = String(song)
         songUL.innerHTML = songUL.innerHTML + `<li><img class="invert" src="music.svg" alt="">
@@ -78,7 +59,67 @@ async function main() {
             // console.log(e.querySelector(".info").firstElementChild.innerHTML)
             playMusic(e.querySelector(".info").firstElementChild.innerHTML.trim())
         })
-    })
+    })     
+    return songs
+}
+
+const playMusic = (track, pause = false)=>{
+    // let audio = new Audio("/songs/" + track)
+    currentSong.src = `/${currfolder}/` + track 
+    if(!pause){
+        currentSong.play()
+        play.src = "pause.svg"
+    }
+    document.querySelector(".songinfo").innerHTML = decodeURI(track)
+    document.querySelector(".songtime").innerHTML = "00:00 / 00:00"
+}
+
+async function displayAlbums(){
+    let a = await fetch(`http://127.0.0.1:3000/songs/`)
+    let response = await a.text()
+    let div = document.createElement("div")
+    div.innerHTML = response
+    let anchors = div.getElementsByTagName("a")
+    let cardContainer = document.querySelector(".cardContainer")    
+    let array = Array.from(anchors)
+    for (let index = 0; index < array.length; index++) {
+        const e = array[index];
+        
+    
+        if(e.href.includes("/songs")){
+            let folder = e.href.split("/").slice(-2)[0]
+            //Get the metadata of the folder
+            let a = await fetch(`http://127.0.0.1:3000/songs/${folder}/info.json`)
+            let response = await a.json()
+            // console.log(response)
+            cardContainer.innerHTML = cardContainer.innerHTML + `<div data-folder="${folder}" class="card">
+                        <div class="play">
+                            <svg width="40" height="40" viewBox="0 0 100 100" xmlns="http://www.w3.org/2000/svg">
+                                <circle cx="50" cy="50" r="40" fill="#1DB954" />
+                                <polygon points="40,35 40,65 70,50" fill="#000000" />
+                            </svg>
+                        </div>
+                        <img src="/songs/${folder}/cover.jpg" alt="">
+                        <h3>${response.title}</h3>
+                        <p>${response.description}</p>
+                    </div>`
+        }
+        
+    }
+        //Load the playlist whenever card is clicked
+        Array.from(document.getElementsByClassName("class")).forEach(e=>{
+            e.addEventListener("click", async item=>{
+                songs = await getSongs(`songs/${item.currentTarget.dataset.folder}`)
+            })
+        })
+}
+
+async function main() {    
+    await getSong("songs/7DS")
+    // console.log(songs)
+    playMusic(songs[0], true) 
+    //Display all the albums on the page
+    await displayAlbums()
 
     //Attach an event listener to play, next and previous 
 
@@ -95,7 +136,7 @@ async function main() {
 
     //Listen for timeupdate event
     currentSong.addEventListener("timeupdate", ()=>{
-        console.log(currentSong.currentTime, currentSong.duration)
+        // console.log(currentSong.currentTime, currentSong.duration)
         document.querySelector(".songtime").innerHTML = `${secondsToMinutesSeconds(currentSong.currentTime)} / ${secondsToMinutesSeconds(currentSong.duration)}` 
         document.querySelector(".circle").style.left = (currentSong.currentTime / currentSong.duration) * 100 + '%'
     })
@@ -107,7 +148,7 @@ async function main() {
         currentSong.currentTime = ((currentSong.duration) * percent )/100
     })
 
-    //Add an event listenerfor hamburger
+    //Add an event listener for hamburger
     document.querySelector(".hamburger").addEventListener("click", ()=>{
         document.querySelector(".left").style.left = "0"
     })
@@ -162,8 +203,7 @@ async function main() {
         }
     })
 
-    //Load the playlist whenever card is clicked
-    
+
 }
 
 main()
